f199be044e43ffc9068e064fead761424a632759
kalebE36 <kaleb.ellington03@gmail.com>
2024-08-18 23:33:50 -0400

    update

--- src/scenes/MainMenu.java
+++ src/scenes/MainMenu.java
@@ -5,6 +5,8 @@ import javafx.scene.control.Button;
 import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 
 public class MainMenu {
@@ -12,23 +14,27 @@ public class MainMenu {
 
     public MainMenu() {
         // Create layout
-        VBox layout = new VBox(20); // VBox with 10px spacing
+        VBox layout = new VBox(20); /* VBox with 20px spacing */ 
         layout.setAlignment(Pos.CENTER);
         
-        // Create buttons
-        Button startButton = new Button("Start Game");
+        Image playButton = new Image(getClass().getResourceAsStream("/res/play_button.jpg"));
+        ImageView playButtonView = new ImageView(playButton);
+        playButtonView.setFitWidth(50);
+        playButtonView.setFitHeight(50);
+        playButtonView.setPreserveRatio(true);
         
+        
+        Button startButton = new Button("Start Game");
+        startButton.setGraphic(playButtonView); 
         
         
         Button loadButton = new Button("Load Game");
         Button exitButton = new Button("Exit");
         
-        // Add buttons to layout
         layout.getChildren().addAll(startButton, loadButton, exitButton);
         exitButton.setOnAction(e -> Platform.exit());
         
-        // Create scene with the layout
-        this.scene = new Scene(layout, 800, 600); // Width 800, Height 600
+        this.scene = new Scene(layout, 800, 600); /* Width 800, Height 600 */
     }
 
     // Getter to access the scene
